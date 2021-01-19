02745580a87ec5029397dfbed801588518d3cf43
GiladVaknin <77235037+GiladVaknin@users.noreply.github.com>
2021-01-19 18:40:22 +0200

    q 1,2

--- q2.js
+++ q2.js
@@ -3,7 +3,12 @@ function run() {
   const output = document.getElementById("output");
   let out = "";
   /**/
-  out = input.isArray;
+  let arr = [1, 2, 3, 4, 5];
+  let arr2 = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr2.push(arr[i]);
+  }
+  console.log(arr2);
   /**/
   output.innerText = out;
 }
